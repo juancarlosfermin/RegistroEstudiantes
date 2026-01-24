6728aa9342b7dfc3c4e47d176e8aee1c37030a67
JuanCarlos <JuanCarlos@DESKTOP-1H4K3BB>
2026-01-24 10:42:20 -0400

    Aqui estan las migraciones correspondientes

---
+++ RegistroEstudiantes/Migrations/20260123033024_InicialAsignaturas.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroEstudiantes.Migrations
+{
+    /// <inheritdoc />
+    public partial class InicialAsignaturas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "asignaturas",
+                columns: table => new
+                {
+                    AsignaturaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Codigo = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Aula = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Creditos = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_asignaturas", x => x.AsignaturaId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "asignaturas");
+        }
+    }
+}
