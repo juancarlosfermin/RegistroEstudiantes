e67267a43f41b80ff5fde96ede0ab190f3c71a9c
JuanCarlos <JuanCarlos@DESKTOP-1H4K3BB>
2026-01-24 10:41:06 -0400

    Este es el contexto donde modifique y agregue lo necesario para que asignatura este funcionando

--- RegistroEstudiantes/DAL/Contexto.cs
+++ RegistroEstudiantes/DAL/Contexto.cs
@@ -9,8 +9,13 @@ public class Contexto : DbContext
 
     public DbSet<EstudianteEntity> Estudiantes { get; set; }
 
+    public DbSet<AsignaturaEntity> Asignaturas { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<EstudianteEntity>().ToTable("estudiantes");
+
+        modelBuilder.Entity<AsignaturaEntity>().ToTable("asignaturas");
     }
 }
